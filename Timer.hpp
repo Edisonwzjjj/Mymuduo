2ab5f113a8f3bcf439d9ce41a12da9dd0f457f9a
Edisonwzjjj <wangzijun2023@outlook.com>
2024-02-13 20:38:47 +1100

    fix bugs in timer

--- Timer.hpp
+++ Timer.hpp
@@ -1,35 +1,34 @@
 #pragma once
 
 #include <iostream>
-#include <list>
-#include <utility>
-#include <vector>
-#include <unordered_set>
 #include <functional>
-#include <memory>
-#include <unordered_map>
-#include <cassert>
-#include <unistd.h>
+#include <utility>
+
 
 using TaskFunc = std::function<void()>;
 using ReleaseFunc = std::function<void()>;
-class TimerTask{
+
+
+class TimerTask {
 private:
-    uint64_t _id;       // 定时器任务对象ID
-    uint32_t _timeout;  //定时任务的超时时间
-    bool _canceled;     // false-表示没有被取消， true-表示被取消
-    TaskFunc _task_cb;  //定时器对象要执行的定时任务
-    ReleaseFunc _release; //用于删除TimerWheel中保存的定时器对象信息
+    uint64_t id_;
+    uint32_t timeout_;
+    bool canceled_{false};
+    TaskFunc task_cb_;
+    ReleaseFunc release_cb_;
+
 public:
-    TimerTask(uint64_t id, uint32_t delay, const TaskFunc &cb):
-            _id(id), _timeout(delay), _task_cb(cb), _canceled(false) {}
+    TimerTask(uint64_t id, uint32_t timeout, TaskFunc cb) :
+            id_(id), timeout_(timeout), task_cb_(std::move(cb)) {}
+
     ~TimerTask() {
-        if (_canceled == false) _task_cb();
-        _release();
+        if (!canceled_) task_cb_();
+        release_cb_();
     }
-    void Cancel() { _canceled = true; }
-    void SetRelease(const ReleaseFunc &cb) { _release = cb; }
-    uint32_t DelayTime() { return _timeout; }
+
+    void Cancel() { canceled_ = true; }
+
+    void SetRelease(ReleaseFunc cb) { release_cb_ = std::move(cb); }
+
+    uint32_t Timeout() const { return timeout_; }
 };
-
-
