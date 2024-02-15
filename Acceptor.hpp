fe9937e71d9612cbce3392d4716704c7cb5d1214
Edisonwzjjj <wangzijun2023@outlook.com>
2024-02-15 17:42:20 +1100

    fix bugs

--- Acceptor.hpp
+++ Acceptor.hpp
@@ -9,34 +9,34 @@
 
 class Acceptor {
 private:
-    Socket _socket;//用于创建监听套接字
-    EventLoop *_loop; //用于对监听套接字进行事件监控
-    Channel _channel; //用于对监听套接字进行事件管理
+    Socket sock_;//用于创建监听套接字
+    EventLoop *loop_; //用于对监听套接字进行事件监控
+    Channel ch_; //用于对监听套接字进行事件管理
 
     using AcceptCallback = std::function<void(int)>;
     AcceptCallback _accept_callback;
 private:
     /*监听套接字的读事件回调处理函数---获取新连接，调用_accept_callback函数进行新连接处理*/
     void HandleRead() {
-        int newfd = _socket.Accept();
-        if (newfd < 0) {
-            return ;
+        int fd = sock_.Accept();
+        if (fd < 0) {
+            return;
         }
-        if (_accept_callback) _accept_callback(newfd);
+        if (_accept_callback) _accept_callback(fd);
     }
     int CreateServer(int port) {
-        bool ret = _socket.CreateServer(port);
-        assert(ret == true);
-        return _socket.Fd();
+        bool ret = sock_.CreateServer(port);
+        assert(ret);
+        return sock_.Fd();
     }
 public:
     /*不能将启动读事件监控，放到构造函数中，必须在设置回调函数后，再去启动*/
     /*否则有可能造成启动监控后，立即有事件，处理的时候，回调函数还没设置：新连接得不到处理，且资源泄漏*/
-    Acceptor(EventLoop *loop, int port): _socket(CreateServer(port)), _loop(loop),
-                                         _channel(loop, _socket.Fd()) {
-        _channel.SetReadCallback(std::bind(&Acceptor::HandleRead, this));
+    Acceptor(EventLoop *loop, int port): sock_(CreateServer(port)), loop_(loop),
+                                         ch_(loop, sock_.Fd()) {
+        ch_.SetReadCallback([this] { HandleRead(); });
     }
     void SetAcceptCallback(const AcceptCallback &cb) { _accept_callback = cb; }
-    void Listen() { _channel.EnableRead(); }
+    void Listen() { ch_.EnableRead(); }
 };
 
--- Buffer.hpp
+++ Buffer.hpp
@@ -7,40 +7,47 @@
 #include <cstdint>
 #include <string>
 #include <iostream>
-#include <cstring>
 
 #define BUFFER_DEFAULT_SIZE 1024
+
 class Buffer {
 private:
-    std::vector<char> _buffer; //使用vector进行内存空间管理
-    uint64_t _reader_idx; //读偏移
-    uint64_t _writer_idx; //写偏移
+    std::vector<char> buf_; //使用vector进行内存空间管理
+    uint64_t read_idx_{0}; //读偏移
+    uint64_t write_idx_{0}; //写偏移
 public:
-    Buffer():_reader_idx(0), _writer_idx(0), _buffer(BUFFER_DEFAULT_SIZE){}
-    char *Begin() { return &*_buffer.begin(); }
-    //获取当前写入起始地址, _buffer的空间起始地址，加上写偏移量
-    char *WritePosition() { return Begin() + _writer_idx; }
-    //获取当前读取起始地址
-    char *ReadPosition() { return Begin() + _reader_idx; }
+    Buffer() : buf_(BUFFER_DEFAULT_SIZE) {}
+
+    auto Begin() -> char * { return &*buf_.begin(); }
+
+    auto WritePosition() -> char * { return Begin() + write_idx_; }
+
+    auto ReadPosition() -> char * { return Begin() + read_idx_; }
+
     //获取缓冲区末尾空闲空间大小--写偏移之后的空闲空间, 总体空间大小减去写偏移
-    uint64_t TailIdleSize() { return _buffer.size() - _writer_idx; }
+    uint64_t TailIdleSize() { return buf_.size() - write_idx_; }
+
     //获取缓冲区起始空闲空间大小--读偏移之前的空闲空间
-    uint64_t HeadIdleSize() { return _reader_idx; }
+    uint64_t HeadIdleSize() const { return read_idx_; }
+
     //获取可读数据大小 = 写偏移 - 读偏移
-    uint64_t ReadAbleSize() { return _writer_idx - _reader_idx; }
+    uint64_t ReadableSize() const { return write_idx_ - read_idx_; }
+
     //将读偏移向后移动
     void MoveReadOffset(uint64_t len) {
-        if (len == 0) return;
+        if (!len) return;
         //向后移动的大小，必须小于可读数据大小
-        assert(len <= ReadAbleSize());
-        _reader_idx += len;
+        assert(len <= ReadableSize());
+        read_idx_ += len;
     }
+
     //将写偏移向后移动
     void MoveWriteOffset(uint64_t len) {
         //向后移动的大小，必须小于当前后边的空闲空间大小
         assert(len <= TailIdleSize());
-        _writer_idx += len;
+        write_idx_ += len;
     }
+
     //确保可写空间足够（整体空闲空间够了就移动数据，否则就扩容）
     void EnsureWriteSpace(uint64_t len) {
         //如果末尾空闲空间大小足够，直接返回
@@ -48,89 +55,59 @@ public:
         //末尾空闲空间不够，则判断加上起始位置的空闲空间大小是否足够, 够了就将数据移动到起始位置
         if (len <= TailIdleSize() + HeadIdleSize()) {
             //将数据移动到起始位置
-            uint64_t rsz = ReadAbleSize();//把当前数据大小先保存起来
+            uint64_t rsz = ReadableSize();//把当前数据大小先保存起来
             std::copy(ReadPosition(), ReadPosition() + rsz, Begin());//把可读数据拷贝到起始位置
-            _reader_idx = 0;    //将读偏移归0
-            _writer_idx = rsz;  //将写位置置为可读数据大小， 因为当前的可读数据大小就是写偏移量
-        }else {
+            read_idx_ = 0;    //将读偏移归0
+            write_idx_ = rsz;  //将写位置置为可读数据大小， 因为当前的可读数据大小就是写偏移量
+        } else {
             //总体空间不够，则需要扩容，不移动数据，直接给写偏移之后扩容足够空间即可
-            DBG_LOG("RESIZE %ld", _writer_idx + len);
-            _buffer.resize(_writer_idx + len);
+            DBG_LOG("RESIZE %ld", write_idx_ + len);
+            buf_.resize(write_idx_ + len);
         }
     }
-    //写入数据
+
+
     void Write(const void *data, uint64_t len) {
         //1. 保证有足够空间，2. 拷贝数据进去
         if (len == 0) return;
         EnsureWriteSpace(len);
-        const char *d = (const char *)data;
+        auto d = static_cast<const char *>(data);
         std::copy(d, d + len, WritePosition());
-    }
-    void WriteAndPush(const void *data, uint64_t len) {
-        Write(data, len);
         MoveWriteOffset(len);
     }
-    void WriteString(const std::string &data) {
-        return Write(data.c_str(), data.size());
-    }
-    void WriteStringAndPush(const std::string &data) {
-        WriteString(data);
-        MoveWriteOffset(data.size());
-    }
-    void WriteBuffer(Buffer &data) {
-        return Write(data.ReadPosition(), data.ReadAbleSize());
-    }
-    void WriteBufferAndPush(Buffer &data) {
-        WriteBuffer(data);
-        MoveWriteOffset(data.ReadAbleSize());
-    }
-    //读取数据
+
+
     void Read(void *buf, uint64_t len) {
-        //要求要获取的数据大小必须小于可读数据大小
-        assert(len <= ReadAbleSize());
-        std::copy(ReadPosition(), ReadPosition() + len, (char*)buf);
-    }
-    void ReadAndPop(void *buf, uint64_t len) {
-        Read(buf, len);
+        assert(len <= ReadableSize());
+        std::copy(ReadPosition(), ReadPosition() + len, (char *) buf);
         MoveReadOffset(len);
     }
-    std::string ReadAsString(uint64_t len) {
+
+public:
+    void Write(std::string_view &str) {
+        return Write(str.data(), str.size());
+    }
+
+    std::string Read(uint64_t len) {
         //要求要获取的数据大小必须小于可读数据大小
-        assert(len <= ReadAbleSize());
+        assert(len <= ReadableSize());
         std::string str;
         str.resize(len);
-        Read(&str[0], len);
+        Read(str.data(), len);
         return str;
     }
-    std::string ReadAsStringAndPop(uint64_t len) {
-        assert(len <= ReadAbleSize());
-        std::string str = ReadAsString(len);
-        MoveReadOffset(len);
-        return str;
-    }
-    char *FindCRLF() {
-        char *res = (char*)memchr(ReadPosition(), '\n', ReadAbleSize());
-        return res;
-    }
-    /*通常获取一行数据，这种情况针对是*/
+
     std::string GetLine() {
-        char *pos = FindCRLF();
-        if (pos == NULL) {
+        auto pos = std::find(ReadPosition(), ReadPosition() + ReadableSize() + 1, '\n');
+        if (!pos) {
             return "";
         }
-        // +1是为了把换行字符也取出来。
-        return ReadAsString(pos - ReadPosition() + 1);
+        return Read(pos - ReadPosition() + 1);
     }
-    std::string GetLineAndPop() {
-        std::string str = GetLine();
-        MoveReadOffset(str.size());
-        return str;
-    }
-    //清空缓冲区
+
     void Clear() {
-        //只需要将偏移量归0即可
-        _reader_idx = 0;
-        _writer_idx = 0;
+        read_idx_ = 0;
+        write_idx_ = 0;
     }
 };
 
