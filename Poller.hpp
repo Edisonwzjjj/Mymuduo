3ca06839fd54984062fb4631d5d571215014e531
Edisonwzjjj <wangzijun2023@outlook.com>
2024-02-16 19:34:53 +1100

    fix bugs

--- Poller.hpp
+++ Poller.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #include "Log.hpp"
 #include "Channel.hpp"
 #include <unordered_map>
@@ -11,82 +10,71 @@
 
 #define MAX_EPOLLEVENTS 1024
 
-
 class Poller {
 private:
-    int _epfd;
-    struct epoll_event _evs[MAX_EPOLLEVENTS];
-    std::unordered_map<int, Channel *> _channels;
-private:
-    //对epoll的直接操作
-    void Update(Channel *channel, int op) {
-        // int epoll_ctl(int epfd, int op,  int fd,  struct epoll_event *ev);
-        int fd = channel->Fd();
-        struct epoll_event ev;
-        ev.data.fd = fd;
-        ev.events = channel->Events();
-        int ret = epoll_ctl(_epfd, op, fd, &ev);
+    int epfd_;
+    struct epoll_event epev_[MAX_EPOLLEVENTS]{};
+    std::unordered_map<int, Channel *> channels_;
+
+public:
+    void Update(Channel *ch, int op) {
+        int fd = ch->Fd();
+        struct epoll_event epev{};
+        epev.data.fd = fd;
+        epev.events = ch->Events();
+        int ret = epoll_ctl(epfd_, op, fd, epev_);
         if (ret < 0) {
             ERR_LOG("EPOLLCTL FAILED!");
         }
-        return;
     }
-    //判断一个Channel是否已经添加了事件监控
-    bool HasChannel(Channel *channel) {
-        auto it = _channels.find(channel->Fd());
-        if (it == _channels.end()) {
-            return false;
-        }
-        return true;
+
+    bool HasChannel(Channel *ch) {
+        auto it = channels_.find(ch->Fd());
+        return it != channels_.end();
     }
+
 public:
     Poller() {
-        _epfd = epoll_create(MAX_EPOLLEVENTS);
-        if (_epfd < 0) {
+        epfd_ = epoll_create(MAX_EPOLLEVENTS);
+        if (epfd_ < 0) {
             ERR_LOG("EPOLL CREATE FAILED!!");
-            abort();//退出程序
+            abort();
         }
     }
-    //添加或修改监控事件
-    void UpdateEvent(Channel *channel) {
-        bool ret = HasChannel(channel);
-        if (ret == false) {
-            //不存在则添加
-            _channels.insert(std::make_pair(channel->Fd(), channel));
-            return Update(channel, EPOLL_CTL_ADD);
-        }
-        return Update(channel, EPOLL_CTL_MOD);
+
+    void UpdateEvent(Channel *ch) {
+        bool st = HasChannel(ch);
+        if (!st) {
+            channels_.insert({ch->Fd(), ch});
+            Update(ch, EPOLL_CTL_ADD);
+        } else Update(ch, EPOLL_CTL_MOD);
     }
-    //移除监控
-    void RemoveEvent(Channel *channel) {
-        auto it = _channels.find(channel->Fd());
-        if (it != _channels.end()) {
-            _channels.erase(it);
+
+    void RemoveEvent(Channel *ch) {
+        bool st = HasChannel(ch);
+        if (st) {
+            channels_.erase(ch->Fd());
         }
-        Update(channel, EPOLL_CTL_DEL);
+        Update(ch, EPOLL_CTL_DEL);
     }
-    //开始监控，返回活跃连接
-    void Poll(std::vector<Channel*> *active) {
-        // int epoll_wait(int epfd, struct epoll_event *evs, int maxevents, int timeout)
-        int nfds = epoll_wait(_epfd, _evs, MAX_EPOLLEVENTS, -1);
+
+    void Poll(std::vector<Channel *> &actives) {
+        int nfds = epoll_wait(epfd_, epev_, MAX_EPOLLEVENTS, -1);
         if (nfds < 0) {
             if (errno == EINTR) {
-                return ;
+                return;
             }
             ERR_LOG("EPOLL WAIT ERROR:%s\n", strerror(errno));
-            abort();//退出程序
+            abort();
         }
-        for (int i = 0; i < nfds; i++) {
-            auto it = _channels.find(_evs[i].data.fd);
-            assert(it != _channels.end());
-            it->second->SetREvents(_evs[i].events);//设置实际就绪的事件
-            active->push_back(it->second);
+        for (int i = 0; i < nfds; ++i) {
+            auto it = channels_.find(epev_[i].data.fd);
+            assert(it != channels_.end());
+            it->second->SetREvents(epev_[i].events);
+            actives.emplace_back(it->second);
         }
-        return;
     }
 };
-
-
 
 
 
