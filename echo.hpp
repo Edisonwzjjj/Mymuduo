bec86d55df054a72e535027b38c4c6fb6658f017
Edisonwzjjj <wangzijun2023@outlook.com>
2024-03-05 13:41:58 +1100

    HttpServer fini

--- echo.hpp
+++ echo.hpp
@@ -18,12 +18,12 @@ class Echo{
             conn->Shutdown();
         }
     public:
-        Echo(int port):_server(port) {
+        explicit Echo(int port):_server(port) {
             _server.SetThreadCount(std::thread::hardware_concurrency());
             _server.EnableInactiveRelease(10);
-            _server.SetClosedCallback(std::bind(&Echo::OnClosed, this, std::placeholders::_1));
-            _server.SetConnectedCallback(std::bind(&Echo::OnConnected, this, std::placeholders::_1));
-            _server.SetMessageCallback(std::bind(&Echo::OnMessage, this, std::placeholders::_1, std::placeholders::_2));
+            _server.SetClosedCallback([this](auto && PH1) { OnClosed(std::forward<decltype(PH1)>(PH1)); });
+            _server.SetConnectedCallback([this](auto && PH1) { OnConnected(std::forward<decltype(PH1)>(PH1)); });
+            _server.SetMessageCallback([this](auto && PH1, auto && PH2) { OnMessage(std::forward<decltype(PH1)>(PH1), std::forward<decltype(PH2)>(PH2)); });
         }
         void Start() { _server.Start(); }
 };
